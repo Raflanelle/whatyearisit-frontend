ff4ea25e0f8be502e216cb0cb413d34ed7f097e3
raphaelle <raflanelle@hotmail.com>
2024-06-28 14:50:59 +0200

    frontend premier commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//affichage de l'année
+fetch('http://localhost:3000/year').then(response => response.json())
+.then(data => {
+    console.log(data);
+    document.querySelector('#year').textContent = data.year;
+});
